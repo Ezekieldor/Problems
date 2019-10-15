1348f0fe6784e201bd36a3b74450f07719ba5441
Ezequiel Rodrigues da Silva <ezequiel.jsd@gmail.com>
2019-10-14 21:20:42 -0300

    Create  2519.cpp

---
+++ 2519.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+#define MAXN 1110
+
+using namespace std;
+
+int ft[MAXN][MAXN];
+
+int last (int x)
+{
+	return x & (-x);
+}
+
+void update (int x, int y, int value)
+{
+	for (int i = x; i < MAXN; i += last (i))
+	{
+		for (int j = y; j < MAXN; j += last (j))
+		{
+			ft[i][j] += value;
+		}
+	}
+}
+
+int query (int x, int y)
+{
+	int value = 0;
+	for (int i = x; i > 0; i -= last (i))
+	{
+		for (int j = y; j > 0; j -= last (j))
+		{
+			value += ft[i][j];
+		}
+	}
+	
+	return value;
+}
+
+int main ()
+{
+	int n, m, a, a1, b1, a2, b2, q, c;
+	while (scanf ("%d%d", &n, &m) != EOF)
+	{
+		memset (ft, 0, sizeof ft);
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 1; j <= m; j++)
+			{
+				scanf ("%d", &a);
+				update (i, j, a);
+			}
+		}
+		//cout << query (2, 4) << endl;
+		scanf ("%d", &q);
+		for (int i = 0; i < q; i++)
+		{
+			scanf ("%d", &c);
+			if (c == 0)
+			{
+				scanf ("%d%d%d%d", &a1, &b1, &a2, &b2);
+				update (a1, b1, -1);
+				update (a2, b2, 1);
+			}
+			else
+			{
+				scanf ("%d%d%d%d", &a1, &b1, &a2, &b2);
+				int v1 = query (a1 - 1, b1 - 1);
+				int v2 = query (a1 - 1, b2);
+				int v3 = query (a2, b1 - 1) - v1;
+				//cout << v1 << " " << v2 << " " << v3 << endl;
+				printf ("%d\n", query (a2, b2) - (v2 + v3));
+			}
+		}
+	}
+}
